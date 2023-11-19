eed775fe5e4d4138ec537674775b49eff36dd782
Jack-Hewn <67242053+Jack-Hewn@users.noreply.github.com>
2023-11-18 21:53:11 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,3 @@
-import { Configuration, OpenAIApi } from 'openai';
-export class OpenAI {
-    constructor(apiKey) {
-        // Create the Configuration and OpenAIApi instances
-        this.openai = new OpenAIApi(new Configuration({ apiKey }));
-    }
-    // Asynchronous function to generate text from the OpenAI API
-    async generateText(prompt, model, max_tokens, temperature = 0.85) {
-        try {
-            // Send a request to the OpenAI API to generate text
-            const response = await this.openai.createCompletion({
-                model,
-                prompt,
-                max_tokens,
-                n: 1,
-                temperature,
-            });
-            console.log(`request cost: ${response.data.usage.total_tokens} tokens`);
-               // Return the text of the response
-            return response.data.choices[0].text;
-        } catch (error) {
-            throw error;
-        }
-    }
-}
-
 var HTTPS = require('https');
 var cool = require('cool-ascii-faces');
 
@@ -31,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^AI$/;
+      botRegex = /^\Execute$/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
