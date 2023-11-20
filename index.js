389c38c4ec8d38fcd203bb1648e4b168c35e9937
Jack-Hewn <67242053+Jack-Hewn@users.noreply.github.com>
2023-11-20 16:41:16 -0600

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
+async function main() {
+  const chatCompletion = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: [
+      { role: 'user', content: 'What is the capital of Texas?' }
+      ]
+  });
+
+  console.log(chatCompletion.data.choices[0]);
+}
+
+main();
+
 var http, director, cool, bot, router, server, port;
 
 http        = require('http');
